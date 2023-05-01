f2d7310c932bf5b4e150b26228c03f09505156da
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-05-01 17:20:17 +0200

    Add project files.

---
+++ PlaybookCsharp/LoopDesign.md
@@ -0,0 +1,9 @@
+## Loop Design
+
+- `for each` loop - for sequences
+- `while` loop - for unknown number of iterations
+- `for` - for making use of the index
+- `for` is equivalent to `while`
+
+## For Complex Loops
+- yield return to seperate iteration logic from results processing
---
+++ PlaybookCsharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+
+namespace PlaybookCsharp
+{
+    internal class Program
+    {        
+        static void Main(string[] args)            
+        {
+            //loop design
+            List<string> groceries = new List<string>() { "Milk", "Eggs", "Bread", "Cheese", "Ham", "Butter" };
+
+            foreach (string item in groceries.Take(3))
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----");
+
+            foreach (string item in groceries.Skip(3))
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----");
+
+            foreach (string item in groceries.Take(3).Skip(1))
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----");
+
+            string filePath1 = Assembly.GetExecutingAssembly().Location;
+            string filePath2 = Assembly.GetEntryAssembly()!.Location;
+            Console.WriteLine(filePath1);
+            Console.WriteLine(filePath2);
+        }
+    }
+}
